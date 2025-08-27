157bdc70210e39f50a2108e18bfab9cbecb16048
唐万龙 <tangwanlong425@qq.com>
2025-08-27 10:57:32 +0800

    add corners

--- Sources/Extension/UIView+TWL.swift
+++ Sources/Extension/UIView+TWL.swift
@@ -116,6 +116,32 @@ public extension UIView {
                 subview.removeFromSuperview()
             }
         }
+
+        public func setCorners(topLeft: CGFloat, topRight: CGFloat, bottomLeft: CGFloat, bottomRight: CGFloat) {
+            let path = UIBezierPath()
+            let bounds = self.view.bounds
+
+            // 依次描点
+            path.move(to: CGPoint(x: bounds.minX + topLeft, y: bounds.minY))
+            path.addLine(to: CGPoint(x: bounds.maxX - topRight, y: bounds.minY))
+            path.addArc(withCenter: CGPoint(x: bounds.maxX - topRight, y: bounds.minY + topRight), radius: topRight,
+                        startAngle: CGFloat(3 * Double.pi / 2), endAngle: 0, clockwise: true)
+            path.addLine(to: CGPoint(x: bounds.maxX, y: bounds.maxY - bottomRight))
+            path.addArc(withCenter: CGPoint(x: bounds.maxX - bottomRight, y: bounds.maxY - bottomRight), radius: bottomRight,
+                        startAngle: 0, endAngle: CGFloat(Double.pi / 2), clockwise: true)
+            path.addLine(to: CGPoint(x: bounds.minX + bottomLeft, y: bounds.maxY))
+            path.addArc(withCenter: CGPoint(x: bounds.minX + bottomLeft, y: bounds.maxY - bottomLeft), radius: bottomLeft,
+                        startAngle: CGFloat(Double.pi / 2), endAngle: CGFloat(Double.pi), clockwise: true)
+            path.addLine(to: CGPoint(x: bounds.minX, y: bounds.minY + topLeft))
+            path.addArc(withCenter: CGPoint(x: bounds.minX + topLeft, y: bounds.minY + topLeft), radius: topLeft,
+                        startAngle: CGFloat(Double.pi), endAngle: CGFloat(3 * Double.pi / 2), clockwise: true)
+            path.close()
+
+            let maskLayer = CAShapeLayer()
+            maskLayer.frame = self.view.bounds
+            maskLayer.path = path.cgPath
+            self.view.layer.mask = maskLayer
+        }
     }
     
     // MARK: TWLUIViewClassExStruct
