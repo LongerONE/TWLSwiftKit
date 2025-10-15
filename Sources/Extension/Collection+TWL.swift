62b30d473ad7c58538fb37777956c180d0982f5d
唐万龙 <tangwanlong425@qq.com>
2025-10-15 15:09:35 +0800

    Collection to json string.

--- Sources/Extension/Collection+TWL.swift
+++ Sources/Extension/Collection+TWL.swift
@@ -10,5 +10,52 @@ public extension Collection {
         return indices.contains(index) ? self[index] : nil
     }
     
+     var twl: TWLCollectionStruct {
+        return TWLCollectionStruct(self)
+    }
     
+}
+
+
+
+public struct TWLCollectionStruct {
+    private let collection: Collection
+    
+    init(_ collection: Collection) {
+        self.collection = collection
+    }
+    
+    public var jsonString: String? {
+        get {
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: self.collection, options: .fragmentsAllowed)
+                if let jsonString = String(data: jsonData, encoding: .utf8) {
+                    return jsonString
+                }
+            } catch {
+                TWLDPrint("Error converting collection to JSON: \(error)")
+            }
+            
+            return nil
+        }
+        
+        set {}
+    }
+    
+    public var prettyJsonString: String? {
+        get {
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: self.collection, options: .prettyPrinted)
+                if let jsonString = String(data: jsonData, encoding: .utf8) {
+                    return jsonString
+                }
+            } catch {
+                TWLDPrint("Error converting collection to JSON: \(error)")
+            }
+            
+            return nil
+        }
+        
+        set {}
+    }
 }
